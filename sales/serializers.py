96c9a185a125a222307808d5e0f1dc2117ef9b86
Moisés <moisesavilar0@gmail.com>
2024-03-02 03:23:16 -0300

    new field in SaleSerializer

--- sales/serializers.py
+++ sales/serializers.py
@@ -3,7 +3,8 @@ from sales.models import Sale
 
 
 class SaleSerializer(serializers.ModelSerializer):
+    customer_name = serializers.CharField(source='customer')
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'product', 'price', 'quantity', 'customer_name')
         model = Sale
